fe1528c9062f6b03815ea33f695c2b606ef7c452
John Haugeland <stonecypher@gmail.com>
2017-06-04 20:11:40 -0700

    clean up spurious .se; bring tests into line

--- src/js/tests/parse.js
+++ src/js/tests/parse.js
@@ -9,21 +9,26 @@ const jssm = require('../../../build/jssm.es5.js');
 
 describe('parse/1', async it => {
 
-  it('a->b;',   t => t.deepEqual([],    jssm.parse('a->b;')   ));
-  it('a ->b;',  t => t.deepEqual([0],   jssm.parse('a ->b;')  ));
-  it('a-> b;',  t => t.deepEqual([0,1], jssm.parse('a-> b;')  ));
-  it('a -> b;', t => t.deepEqual([0,1], jssm.parse('a -> b;') ));
+  const AtoB = [{"from": "a","se": {"kind": "->","to": "b"}}],
+        AdB  = [{"from": "a","se": {"kind": "->","to": "b","ldesc": [{"text":"c","value":"d"}]}}],
+        ABd  = [{"from": "a","se": {"kind": "->","to": "b","rdesc": [{"text":"c","value":"d"}]}}],
+        AdBd = [{"from": "a","se": {"kind": "->","to": "b","ldesc": [{"text":"c","value":"d"}],"rdesc": [{"text":"e","value":"f"}]}}];
 
-  it('a{}->b;',   t => t.deepEqual([],    jssm.parse('a{}->b;')   ));
-  it('a->{}b;',   t => t.deepEqual([0],   jssm.parse('a->{}b;')   ));
-  it('a{}->{}b;', t => t.deepEqual([0,1], jssm.parse('a{}->{}b;') ));
+  it('a->b;',   t => t.deepEqual(AtoB, jssm.parse('a->b;')   ));
+  it('a ->b;',  t => t.deepEqual(AtoB, jssm.parse('a ->b;')  ));
+  it('a-> b;',  t => t.deepEqual(AtoB, jssm.parse('a-> b;')  ));
+  it('a -> b;', t => t.deepEqual(AtoB, jssm.parse('a -> b;') ));
 
-  it('a{c:d}->b;',     t => t.deepEqual([], jssm.parse('a{c:d;}->b;')       ));
-  it('a{c:"d"}->b;',   t => t.deepEqual([], jssm.parse('a{c:"d";}->b;')     ));
-  it('a{"c":d}->b;',   t => t.deepEqual([], jssm.parse('a{"c":d;}->b;')     ));
-  it('a{"c":"d"}->b;', t => t.deepEqual([], jssm.parse('a{"c":"d";}->b;')   ));
-  it('a{c:d}->b;',     t => t.deepEqual([], jssm.parse('a->{c:d;}b;')       ));
-  it('a{c:d}->b;',     t => t.deepEqual([], jssm.parse('a{c:d;}->{e:f;}b;') ));
+  it('a{}->b;',   t => t.deepEqual(AtoB, jssm.parse('a{}->b;')   ));
+  it('a->{}b;',   t => t.deepEqual(AtoB, jssm.parse('a->{}b;')   ));
+  it('a{}->{}b;', t => t.deepEqual(AtoB, jssm.parse('a{}->{}b;') ));
+
+  it('a{c:d}->b;',     t => t.deepEqual(AdB,  jssm.parse('a{c:d;}->b;')       ));
+  it('a{c:"d"}->b;',   t => t.deepEqual(AdB,  jssm.parse('a{c:"d";}->b;')     ));
+  it('a{"c":d}->b;',   t => t.deepEqual(AdB,  jssm.parse('a{"c":d;}->b;')     ));
+  it('a{"c":"d"}->b;', t => t.deepEqual(AdB,  jssm.parse('a{"c":"d";}->b;')   ));
+  it('a{c:d}->b;',     t => t.deepEqual(ABd,  jssm.parse('a->{c:d;}b;')       ));
+  it('a{c:d}->b;',     t => t.deepEqual(AdBd, jssm.parse('a{c:d;}->{e:f;}b;') ));
 
 });
 
