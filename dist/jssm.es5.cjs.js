e7e2637bb0e02b496981c3af00209a0ce47755aa
John Haugeland <stonecypher@gmail.com>
2017-06-25 22:57:54 -0700

    ditch hexo

--- dist/jssm.es5.cjs.js
+++ dist/jssm.es5.cjs.js
@@ -2673,7 +2673,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var version = '2.6.0'; // replaced from package.js in build
+var version = '2.6.1'; // replaced from package.js in build
 
 
 // whargarbl lots of these return arrays could/should be sets
