bad6b32a87aa3bc3293a1cd1172dc6c27cb55f54
John Haugeland <stonecypher@gmail.com>
2017-07-03 16:42:45 -0700

    nyc and del-cli were out of date

--- dist/jssm.es5.cjs.js
+++ dist/jssm.es5.cjs.js
@@ -3198,7 +3198,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var version = '2.9.5'; // replaced from package.js in build
+var version = '2.9.6'; // replaced from package.js in build
 
 
 // whargarbl lots of these return arrays could/should be sets
