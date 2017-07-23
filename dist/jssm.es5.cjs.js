c14cde2064a7ba4c0c6fe6394abce6b730c54e34
John Haugeland <stonecypher@gmail.com>
2017-07-23 12:46:13 -0700

    fat arrows, tilde arrows, node lists

--- dist/jssm.es5.cjs.js
+++ dist/jssm.es5.cjs.js
@@ -3120,60 +3120,60 @@ module.exports = {
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var weighted_rand_select = function weighted_rand_select(options) {
-    var probability_property = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'probability';
+  var probability_property = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'probability';
 
 
-    if (!Array.isArray(options)) {
-        throw new TypeError('options must be a non-empty array of objects');
-    }
+  if (!Array.isArray(options)) {
+    throw new TypeError('options must be a non-empty array of objects');
+  }
 
-    if (!(_typeof(options[0]) === 'object')) {
-        throw new TypeError('options must be a non-empty array of objects');
-    }
+  if (!(_typeof(options[0]) === 'object')) {
+    throw new TypeError('options must be a non-empty array of objects');
+  }
 
-    var frand = function frand(cap) {
-        return Math.random() * cap;
-    },
-        prob_sum = options.reduce(function (acc, val) {
-        return acc + val[probability_property];
-    }, 0),
-        rnd = frand(prob_sum);
+  var frand = function frand(cap) {
+    return Math.random() * cap;
+  },
+      prob_sum = options.reduce(function (acc, val) {
+    return acc + val[probability_property];
+  }, 0),
+      rnd = frand(prob_sum);
 
-    var cursor = 0,
-        cursor_sum = 0;
+  var cursor = 0,
+      cursor_sum = 0;
 
-    while ((cursor_sum += options[cursor++][probability_property]) <= rnd) {}
-    return options[cursor - 1];
+  while ((cursor_sum += options[cursor++][probability_property]) <= rnd) {}
+  return options[cursor - 1];
 };
 
 var seq = function seq(n) {
-    return new Array(n).fill(true).map(function (_, i) {
-        return i;
-    });
+  return new Array(n).fill(true).map(function (_, i) {
+    return i;
+  });
 };
 
 var histograph = function histograph(a) {
-    return a.sort().reduce(function (m, v) {
-        return m.set(v, m.has(v) ? m.get(v) + 1 : 1), m;
-    }, new Map());
+  return a.sort().reduce(function (m, v) {
+    return m.set(v, m.has(v) ? m.get(v) + 1 : 1), m;
+  }, new Map());
 };
 
 var weighted_sample_select = function weighted_sample_select(n, options, probability_property) {
-    return seq(n).map(function (i) {
-        return weighted_rand_select(options, probability_property);
-    });
+  return seq(n).map(function (i) {
+    return weighted_rand_select(options, probability_property);
+  });
 };
 
 var weighted_histo_key = function weighted_histo_key(n, options, probability_property, extract) {
-    return histograph(weighted_sample_select(n, options, probability_property).map(function (s) {
-        return s[extract];
-    }));
+  return histograph(weighted_sample_select(n, options, probability_property).map(function (s) {
+    return s[extract];
+  }));
 };
 
 exports.seq = seq;
@@ -3186,102 +3186,102 @@ exports.weighted_sample_select = weighted_sample_select;
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+            value: true
 });
 
 var viz = function viz(jssm) {
 
-  var l_states = jssm.states();
+            var l_states = jssm.states();
 
-  var node_of = function node_of(state) {
-    return 'n' + l_states.indexOf(state);
-  },
-      vc = function vc(col) {
-    return jssm._viz_colors[col] || '';
-  };
+            var node_of = function node_of(state) {
+                        return 'n' + l_states.indexOf(state);
+            },
+                vc = function vc(col) {
+                        return jssm._viz_colors[col] || '';
+            };
 
-  var nodes = l_states.map(function (s) {
+            var nodes = l_states.map(function (s) {
 
-    var this_state = jssm.state_for(s),
-        terminal = jssm.state_is_terminal(s),
-        final = jssm.state_is_final(s),
-        complete = jssm.state_is_complete(s),
-        features = [['label', s], ['peripheries', complete ? 2 : 1], ['fillcolor', final ? vc('fill_final') : complete ? vc('fill_complete') : terminal ? vc('fill_terminal') : '']].filter(function (r) {
-      return r[1];
-    }).map(function (r) {
-      return r[0] + '="' + r[1] + '"';
-    }).join(' ');
-    return node_of(s) + ' [' + features + '];';
-  }).join(' ');
+                        var this_state = jssm.state_for(s),
+                            terminal = jssm.state_is_terminal(s),
+                            final = jssm.state_is_final(s),
+                            complete = jssm.state_is_complete(s),
+                            features = [['label', s], ['peripheries', complete ? 2 : 1], ['fillcolor', final ? vc('fill_final') : complete ? vc('fill_complete') : terminal ? vc('fill_terminal') : '']].filter(function (r) {
+                                    return r[1];
+                        }).map(function (r) {
+                                    return r[0] + '="' + r[1] + '"';
+                        }).join(' ');
+                        return node_of(s) + ' [' + features + '];';
+            }).join(' ');
 
-  var strike = [];
-  var edges = jssm.states().map(function (s) {
-    return jssm.list_exits(s).map(function (ex) {
+            var strike = [];
+            var edges = jssm.states().map(function (s) {
+                        return jssm.list_exits(s).map(function (ex) {
 
-      if (strike.find(function (row) {
-        return row[0] === s && row[1] == ex;
-      })) {
-        return ''; // already did the pair
-      }
+                                    if (strike.find(function (row) {
+                                                return row[0] === s && row[1] == ex;
+                                    })) {
+                                                return ''; // already did the pair
+                                    }
 
-      var edge = jssm.list_transitions(s, ex),
-          pair = jssm.list_transitions(ex, s),
-          double = pair && s !== ex,
-          head_state = jssm.state_for(s),
-          tail_state = jssm.state_for(ex),
+                                    var edge = jssm.list_transitions(s, ex),
+                                        pair = jssm.list_transitions(ex, s),
+                                        double = pair && s !== ex,
+                                        head_state = jssm.state_for(s),
+                                        tail_state = jssm.state_for(ex),
 
 
-      //          label        = edge  ? ([edge.name?`${(edge.name:any)}`:undefined,`${(edge.probability:any)}`]
-      //                                 .filter(not_undef => !!not_undef)
-      //                                   .join('\n') || undefined
-      //                                  ) : undefined,
+                                    //          label        = edge  ? ([edge.name?`${(edge.name:any)}`:undefined,`${(edge.probability:any)}`]
+                                    //                                 .filter(not_undef => !!not_undef)
+                                    //                                   .join('\n') || undefined
+                                    //                                  ) : undefined,
 
-      if_obj_field = function if_obj_field(obj, field) {
-        return obj ? obj[field] || '' : '';
-      },
-          h_final = jssm.state_is_final(s),
-          h_complete = jssm.state_is_complete(s),
-          h_terminal = jssm.state_is_terminal(s),
-          t_final = jssm.state_is_final(ex),
-          t_complete = jssm.state_is_complete(ex),
-          t_terminal = jssm.state_is_terminal(ex),
-          lineColor = function lineColor(final, complete, terminal) {
-        var _solo_1_2 = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '_solo';
+                                    if_obj_field = function if_obj_field(obj, field) {
+                                                return obj ? obj[field] || '' : '';
+                                    },
+                                        h_final = jssm.state_is_final(s),
+                                        h_complete = jssm.state_is_complete(s),
+                                        h_terminal = jssm.state_is_terminal(s),
+                                        t_final = jssm.state_is_final(ex),
+                                        t_complete = jssm.state_is_complete(ex),
+                                        t_terminal = jssm.state_is_terminal(ex),
+                                        lineColor = function lineColor(final, complete, terminal) {
+                                                var _solo_1_2 = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '_solo';
 
-        return final ? vc('line_final' + _solo_1_2) : complete ? vc('line_complete' + _solo_1_2) : terminal ? vc('line_terminal' + _solo_1_2) : vc('normal_line' + _solo_1_2);
-      },
-          textColor = function textColor(final, complete, terminal) {
-        var _solo_1_2 = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '_solo';
+                                                return final ? vc('line_final' + _solo_1_2) : complete ? vc('line_complete' + _solo_1_2) : terminal ? vc('line_terminal' + _solo_1_2) : vc('normal_line' + _solo_1_2);
+                                    },
+                                        textColor = function textColor(final, complete, terminal) {
+                                                var _solo_1_2 = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '_solo';
 
-        return final ? vc('text_final' + _solo_1_2) : complete ? vc('text_complete' + _solo_1_2) : terminal ? vc('text_terminal' + _solo_1_2) : '';
-      },
-          headColor = textColor(h_final, h_complete, h_terminal, double ? '_1' : '_solo'),
-          tailColor = textColor(t_final, t_complete, t_terminal, double ? '_2' : '_solo'),
-          labelInline = [
-      //                           [edge, 'name',        'label',     true],
-      [pair, 'probability', 'headlabel', 'name', 'action', double, headColor], [edge, 'probability', 'taillabel', 'name', 'action', true, tailColor]].map(function (r) {
-        return { which: r[2], whether: r[5] ? [if_obj_field(r[0], r[5]), if_obj_field(r[0], r[1]), if_obj_field(r[0], r[3])].filter(function (q) {
-            return q;
-          }).join('<br/>') || '' : '', color: r[6] };
-      }).filter(function (present) {
-        return present.whether;
-      }).map(function (r) {
-        return r.which + '=' + (r.color ? '<<font color="' + r.color + '">' + r.whether + '</font>>' : '"' + r.whether + '"') + ';';
-      }).join(' '),
-          tc1 = lineColor(t_final, t_complete, t_terminal, '_1'),
-          tc2 = lineColor(h_final, h_complete, h_terminal, '_2'),
-          tcd = lineColor(t_final, t_complete, t_terminal, '_solo'),
-          edgeInline = edge ? double ? 'dir=both;color="' + tc1 + ':' + tc2 + '"' : 'color="' + tcd + '"' : '';
+                                                return final ? vc('text_final' + _solo_1_2) : complete ? vc('text_complete' + _solo_1_2) : terminal ? vc('text_terminal' + _solo_1_2) : '';
+                                    },
+                                        headColor = textColor(h_final, h_complete, h_terminal, double ? '_1' : '_solo'),
+                                        tailColor = textColor(t_final, t_complete, t_terminal, double ? '_2' : '_solo'),
+                                        labelInline = [
+                                    //                           [edge, 'name',        'label',     true],
+                                    [pair, 'probability', 'headlabel', 'name', 'action', double, headColor], [edge, 'probability', 'taillabel', 'name', 'action', true, tailColor]].map(function (r) {
+                                                return { which: r[2], whether: r[5] ? [if_obj_field(r[0], r[5]), if_obj_field(r[0], r[1]), if_obj_field(r[0], r[3])].filter(function (q) {
+                                                                        return q;
+                                                            }).join('<br/>') || '' : '', color: r[6] };
+                                    }).filter(function (present) {
+                                                return present.whether;
+                                    }).map(function (r) {
+                                                return r.which + '=' + (r.color ? '<<font color="' + r.color + '">' + r.whether + '</font>>' : '"' + r.whether + '"') + ';';
+                                    }).join(' '),
+                                        tc1 = lineColor(t_final, t_complete, t_terminal, '_1'),
+                                        tc2 = lineColor(h_final, h_complete, h_terminal, '_2'),
+                                        tcd = lineColor(t_final, t_complete, t_terminal, '_solo'),
+                                        edgeInline = edge ? double ? 'dir=both;color="' + tc1 + ':' + tc2 + '"' : 'color="' + tcd + '"' : '';
 
-      if (pair) {
-        strike.push([ex, s]);
-      }
+                                    if (pair) {
+                                                strike.push([ex, s]);
+                                    }
 
-      return node_of(s) + '->' + node_of(ex) + ' [' + labelInline + edgeInline + '];';
-    }).join(' ');
-  }).join(' ');
+                                    return node_of(s) + '->' + node_of(ex) + ' [' + labelInline + edgeInline + '];';
+                        }).join(' ');
+            }).join(' ');
 
-  return 'digraph G {\n  fontname="helvetica neue";\n  style=filled;\n  bgcolor=lightgrey;\n  node [fontsize=14; shape=box; style=filled; fillcolor=white; fontname="helvetica neue"];\n  edge [fontsize=6;fontname="helvetica neue"];\n\n  ' + nodes + '\n\n  ' + edges + '\n}';
+            return 'digraph G {\n  fontname="helvetica neue";\n  style=filled;\n  bgcolor=lightgrey;\n  node [fontsize=14; shape=box; style=filled; fillcolor=white; fontname="helvetica neue"];\n  edge [fontsize=6;fontname="helvetica neue"];\n\n  ' + nodes + '\n\n  ' + edges + '\n}';
 };
 
 exports.viz = viz;
@@ -3414,7 +3414,7 @@ var machine = function () {
             actionMap.set(tr.from, thisEdgeId);
           }
         } else {
-          throw new Error('should be impossible, satisfying type checker that doesn\'t know .set precedes .get.  severe error?');
+          throw new Error('should be impossible, satisfying type checker that doesn\'t know .set precedes .get.  severe error?'); // LCOV_EXCL_LINE
         }
 
         // reverse mapping first by state origin name
@@ -3430,7 +3430,7 @@ var machine = function () {
             rActionMap.set(tr.action, thisEdgeId);
           }
         } else {
-          throw new Error('should be impossible, satisfying type checker that doesn\'t know .set precedes .get again.  severe error?');
+          throw new Error('should be impossible, satisfying type checker that doesn\'t know .set precedes .get again.  severe error?'); // LCOV_EXCL_LINE
         }
 
         // reverse mapping first by state target name
