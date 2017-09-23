d1abf9799bb4133599cb56dfc4c2660a5ae6f897
John Haugeland <stonecypher@gmail.com>
2017-09-22 23:10:27 -0700

    underway

--- src/js/tests/cycles.js
+++ src/js/tests/cycles.js
@@ -3,7 +3,8 @@
 
 import {test, describe} from 'ava-spec';
 
-const jssm = require('../../../build/jssm.es5.js');
+const jssm = require('../../../build/jssm.es5.js'),
+      sm   = jssm.sm;
 
 
 
@@ -27,6 +28,13 @@ describe('cycle strategies', async _it => {
 
   describe('wide cycle', async it => {
     is_v('[a b c] -> +2;', [{from: ['a','b','c'], key: 'transition', se: {kind: '->', to: {key: 'cycle', value: 2}}}], it);
+  });
+
+  describe('full parse', async it => {
+    it('[a b] -> +1;', t => t.deepEqual(
+      sm`[a b] -> +1;`.list_edges(),
+      [{"from":"a","to":"b","kind":"legal","forced_only":false,"main_path":false},{"from":"b","to":"a","kind":"legal","forced_only":false,"main_path":false}]
+    ));
   });
 
   describe('illegal fractional cycle throws', async it => {
