92c8c3cab778f0623eaa5624cfdab1877d0bdf7d
John Haugeland <stonecypher@gmail.com>
2019-05-14 22:04:20 -0700

    fix parse actions

--- src/js/tests/parse actions.js
+++ src/js/tests/parse actions.js
@@ -17,19 +17,19 @@ describe('matter', async it => {
     it( 'starts Solid',    t => t.is('Solid',  matter.state()        ));
     it( 'Heat is true',    t => t.is(true,     matter.action('Heat') ));
     it( 'is now Liquid',   t => t.is('Liquid', matter.state()        ));
-    it( 'Heat is true',    t => t.is(true,     matter.action('Heat') ));
+    it( 'Heat is true 2',  t => t.is(true,     matter.action('Heat') ));
     it( 'is now Gas',      t => t.is('Gas',    matter.state()        ));
-    it( 'Heat is true',    t => t.is(true,     matter.action('Heat') ));
+    it( 'Heat is true 3',  t => t.is(true,     matter.action('Heat') ));
     it( 'is now Plasma',   t => t.is('Plasma', matter.state()        ));
     it( 'Heat is false',   t => t.is(false,    matter.action('Heat') ));
-    it( 'is now Plasma',   t => t.is('Plasma', matter.state()        ));
+    it( 'is now Plasma 2', t => t.is('Plasma', matter.state()        ));
     it( 'Cool is true',    t => t.is(true,     matter.action('Cool') ));
-    it( 'is now Gas',      t => t.is('Gas',    matter.state()        ));
-    it( 'Cool is true',    t => t.is(true,     matter.action('Cool') ));
-    it( 'is now Liquid',   t => t.is('Liquid', matter.state()        ));
-    it( 'Cool is true',    t => t.is(true,     matter.action('Cool') ));
+    it( 'is now Gas 2',    t => t.is('Gas',    matter.state()        ));
+    it( 'Cool is true 2',  t => t.is(true,     matter.action('Cool') ));
+    it( 'is now Liquid 2', t => t.is('Liquid', matter.state()        ));
+    it( 'Cool is true 3',  t => t.is(true,     matter.action('Cool') ));
     it( 'is now Solid',    t => t.is('Solid',  matter.state()        ));
     it( 'Cool is false',   t => t.is(false,    matter.action('Cool') ));
-    it( 'is now Solid',    t => t.is('Solid',  matter.state()        ));
+    it( 'is now Solid 2',  t => t.is('Solid',  matter.state()        ));
 
 });
