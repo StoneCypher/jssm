fbe88ef08bb9880cb0f4edd810993656c8f496bb
John Haugeland <stonecypher@gmail.com>
2021-03-04 07:30:38 -0800

    underway

---
+++ src/buildjs/benchmark.js
@@ -0,0 +1,20 @@
+
+const b    = require('benny'),
+      jssm = require('../../dist/jssm.es5.cjs');
+
+
+
+
+
+b.suite('Example',
+
+  b.add('Reduce two elements',  () => [1, 2].reduce((a, b) => a + b) ),
+  b.add('Reduce five elements', () => { ;[1, 2, 3, 4, 5].reduce((a, b) => a + b) } ),
+
+  b.cycle(),
+  b.complete(),
+
+  b.save({ file: 'reduce', version: '1.0.0' }),
+  b.save({ file: 'reduce', format: 'chart.html' }),
+
+);
