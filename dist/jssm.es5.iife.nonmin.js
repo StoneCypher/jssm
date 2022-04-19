7d12c2fdd60b152518e1214b79dd2e6a60682f2e
John Haugeland <stonecypher@gmail.com>
2022-04-19 12:16:17 -0700

    Okay, it can verify the version for whoever, just gate the bump and release

--- dist/jssm.es5.iife.nonmin.js
+++ dist/jssm.es5.iife.nonmin.js
@@ -15884,7 +15884,7 @@ var jssm = (function (exports) {
       }
   }
 
-  const version = "5.42.0";
+  const version = "5.43.0";
 
   // whargarbl lots of these return arrays could/should be sets
   /* eslint-disable complexity */
--- src/ts/version.ts
+++ src/ts/version.ts
@@ -1,3 +1,3 @@
 
-const version: string = "5.42.0";
+const version: string = "5.43.0";
 export { version };
