7473afdf4caa6cc41abcca9fd88b4b718d714ea2
John Haugeland <stonecypher@gmail.com>
2022-05-22 07:57:33 -0700

    Document from and sm, fixes StoneCypher/fsl#784, fixes StoneCypher/fsl#785

--- dist/es6/jssm.d.ts
+++ dist/es6/jssm.d.ts
@@ -82,13 +82,15 @@ declare function arrow_right_kind(arrow: JssmArrow): JssmArrowKind;
  *  operator `sm`, and mostly falls back to `.from` when needing to parse
  *  strings dynamically instead of from template literals.
  *
+ *  Operator {@link sm}:
+ *
  *  ```typescript
  *  import { sm } from './jssm';
  *
  *  const switch = sm`on <=> off;`;
  *  ```
  *
- *  &hellip; or &hellip;
+ *  Method {@link from}:
  *
  *  ```typescript
  *  import * as jssm from './jssm';
@@ -131,13 +133,15 @@ declare function wrap_parse(input: string, options?: Object): any;
  *  operator `sm`, and mostly falls back to `.from` when needing to parse
  *  strings dynamically instead of from template literals.
  *
+ *  Operator {@link sm}:
+ *
  *  ```typescript
  *  import { sm } from './jssm';
  *
  *  const switch = sm`on <=> off;`;
  *  ```
  *
- *  &hellip; or &hellip;
+ *  Method {@link from}:
  *
  *  ```typescript
  *  import * as jssm from './jssm';
@@ -281,6 +285,39 @@ declare class Machine<mDT> {
     instance_name(): string | undefined;
     sm(template_strings: TemplateStringsArray, ...remainder: any[]): Machine<mDT>;
 }
+/*********
+ *
+ *  Create a state machine from a template string.  This is one of the two main
+ *  paths for working with JSSM, alongside {@link from}.
+ *
+ *  Use this method when you want to work directly and conveniently with a
+ *  constant template expression.  Use `.from` when you want to pull from
+ *  dynamic strings.
+ *
+ *
+ *  ```typescript
+ *  import * as jssm from './jssm';
+ *
+ *  const switch = jssm.from('on <=> off;');
+ *  ```
+ *
+ */
 declare function sm<mDT>(template_strings: TemplateStringsArray, ...remainder: any[]): Machine<mDT>;
+/*********
+ *
+ *  Create a state machine from an implementation string.  This is one of the
+ *  two main paths for working with JSSM, alongside {@link sm}.
+ *
+ *  Use this method when you want to conveniently pull a state machine from a
+ *  string dynamically.  Use operator `sm` when you just want to work with a
+ *  template expression.
+ *
+ *  ```typescript
+ *  import * as jssm from './jssm';
+ *
+ *  const switch = jssm.from('on <=> off;');
+ *  ```
+ *
+ */
 declare function from<mDT>(MachineAsString: string, ExtraConstructorFields?: Partial<JssmGenericConfig<mDT>> | undefined): Machine<mDT>;
 export { version, transfer_state_properties, Machine, make, wrap_parse as parse, compile, sm, from, arrow_direction, arrow_left_kind, arrow_right_kind, seq, weighted_rand_select, histograph, weighted_sample_select, weighted_histo_key, shapes, gviz_shapes, named_colors };
--- dist/jssm.es5.iife.nonmin.js
+++ dist/jssm.es5.iife.nonmin.js
@@ -16731,7 +16731,7 @@ var jssm = (function (exports) {
       }
   }
 
-  const version = "5.65.3";
+  const version = "5.65.4";
 
   class JssmError extends Error {
       constructor(machine, message, JEEI) {
@@ -17009,13 +17009,15 @@ var jssm = (function (exports) {
    *  operator `sm`, and mostly falls back to `.from` when needing to parse
    *  strings dynamically instead of from template literals.
    *
+   *  Operator {@link sm}:
+   *
    *  ```typescript
    *  import { sm } from './jssm';
    *
    *  const switch = sm`on <=> off;`;
    *  ```
    *
-   *  &hellip; or &hellip;
+   *  Method {@link from}:
    *
    *  ```typescript
    *  import * as jssm from './jssm';
@@ -17132,13 +17134,15 @@ var jssm = (function (exports) {
    *  operator `sm`, and mostly falls back to `.from` when needing to parse
    *  strings dynamically instead of from template literals.
    *
+   *  Operator {@link sm}:
+   *
    *  ```typescript
    *  import { sm } from './jssm';
    *
    *  const switch = sm`on <=> off;`;
    *  ```
    *
-   *  &hellip; or &hellip;
+   *  Method {@link from}:
    *
    *  ```typescript
    *  import * as jssm from './jssm';
@@ -17951,6 +17955,23 @@ var jssm = (function (exports) {
           return sm(template_strings, ...remainder);
       }
   }
+  /*********
+   *
+   *  Create a state machine from a template string.  This is one of the two main
+   *  paths for working with JSSM, alongside {@link from}.
+   *
+   *  Use this method when you want to work directly and conveniently with a
+   *  constant template expression.  Use `.from` when you want to pull from
+   *  dynamic strings.
+   *
+   *
+   *  ```typescript
+   *  import * as jssm from './jssm';
+   *
+   *  const switch = jssm.from('on <=> off;');
+   *  ```
+   *
+   */
   function sm(template_strings, ...remainder /* , arguments */) {
       // foo`a${1}b${2}c` will come in as (['a','b','c'],1,2)
       // this includes when a and c are empty strings
@@ -17964,6 +17985,22 @@ var jssm = (function (exports) {
       /* eslint-enable  prefer-rest-params */
       )));
   }
+  /*********
+   *
+   *  Create a state machine from an implementation string.  This is one of the
+   *  two main paths for working with JSSM, alongside {@link sm}.
+   *
+   *  Use this method when you want to conveniently pull a state machine from a
+   *  string dynamically.  Use operator `sm` when you just want to work with a
+   *  template expression.
+   *
+   *  ```typescript
+   *  import * as jssm from './jssm';
+   *
+   *  const switch = jssm.from('on <=> off;');
+   *  ```
+   *
+   */
   function from(MachineAsString, ExtraConstructorFields) {
       const to_decorate = make(MachineAsString);
       if (ExtraConstructorFields !== undefined) {
