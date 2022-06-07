570555ea0c4dcc36e50358152401f7055012b011
John Haugeland <stonecypher@gmail.com>
2022-06-06 22:39:30 -0700

    trivial doc stubs

--- dist/jssm.es5.iife.nonmin.js
+++ dist/jssm.es5.iife.nonmin.js
@@ -16782,7 +16782,7 @@ var jssm = (function (exports) {
       }
   }
 
-  const version = "5.69.0";
+  const version = "5.69.1";
 
   class JssmError extends Error {
       constructor(machine, message, JEEI) {
---
+++ src/doc_md/ExampleMachines.md
@@ -0,0 +1,3 @@
+# Example Machines
+
+This help page has not yet been written.
