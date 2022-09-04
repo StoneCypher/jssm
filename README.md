72c4ac7e754ab32fe3ca2372fff1733ba8363b27
John Haugeland <stonecypher@gmail.com>
2022-09-03 17:16:16 -0700

    Unicode range tests for atoms and strings, very slow, fixes StoneCypher/fsl#1190, fixes StoneCypher/fsl#1191

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Please edit the file it's derived from, instead: `./src/md/readme_base.md`
 
 
 
-* Generated for version 5.82.7 at 9/1/2022, 7:12:41 PM
+* Generated for version 5.82.7 at 9/3/2022, 4:58:30 PM
 
 -->
 # jssm
--- src/ts/tests/unicode.uspec-driver.ts
+++ src/ts/tests/unicode.uspec-driver.ts
@@ -52,59 +52,31 @@ const atom_start = 32,
 
 
 
-function test_range_with(desc: string, func: (number) => boolean) {
+function test_range_with(tmult: number, func: (number) => boolean) {
 
-  describe(`Unicode character tests - ${desc}`, () => {
+  Object.keys(blocks).forEach(blockname => {
 
-    Object.keys(blocks).forEach(blockname => {
-    // const blockname = 'Basic Latin';
+    const lo = blocks[blockname][0],
+          hi = blocks[blockname][1],
+          sz = hi - lo;
 
-      const lo = blocks[blockname][0],
-            hi = blocks[blockname][1],
-            sz = hi - lo;
+    test(`${blockname}, ${sz} ch, ${sz*tmult} tests`, () => {
 
-      describe(`${blockname} [${lo} - ${hi}] (${sz} ch)`, () => {
-
-        // left clause; right clause; middle clause in chain
-        test(`${blockname} in atoms, ${sz*3} tests`, () => {
-
-          for (let idx = Math.max(blocks[blockname][0], atom_start); idx <= blocks[blockname][1]; ++idx) {
-
-            func(idx);
-
-            // const cp = String.fromCodePoint(idx);
-
-            // if (!(atom_skips.includes(cp))) {
-
-            //   let left_test, middle_test, right_test;
-
-            //   try {
-            //     left_test   = sm`${cp} -> target;`,
-            //     middle_test = sm`source -> ${cp} -> target;`,
-            //     right_test  = sm`source -> ${cp};`;
-            //   } catch (e) {
-            //     throw new Error(`Broke on ${idx} "${cp}"`);
-            //   }
-
-            //   expect( left_test.has_state(cp)   ).toBe(true);
-            //   expect( right_test.has_state(cp)  ).toBe(true);
-            //   expect( middle_test.has_state(cp) ).toBe(true);
-
-            // }
-
-          }
-
-        });
-
-        test.todo(`${blockname} in strings`);
-        test.todo(`${blockname} in label atom`);
-        test.todo(`${blockname} in label string`);
-        test.todo(`${blockname} in spread`);
-
-      });
+      for (let idx = Math.max(blocks[blockname][0], atom_start); idx <= blocks[blockname][1]; ++idx) {
+        func(idx);
+      }
 
     });
 
   });
 
 }
+
+
+
+
+
+export {
+  test_range_with,
+  atom_skips, atom_start
+};
