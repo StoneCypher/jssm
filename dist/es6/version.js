373b64acbc6b5714e175f24660d2bc50e14af8c3
John Haugeland <stonecypher@gmail.com>
2022-12-04 19:17:19 -0800

    Removing node 11 and node 12 from the github action over lack of support by MS

--- dist/es6/version.js
+++ dist/es6/version.js
@@ -1,2 +1,2 @@
-const version = "5.88.0", build_time = 1670183010793;
+const version = "5.88.1", build_time = 1670210112706;
 export { version, build_time };
--- src/ts/version.ts
+++ src/ts/version.ts
@@ -1,5 +1,5 @@
 
-const version    : string = "5.88.0",
-      build_time : number = 1670183010793;
+const version    : string = "5.88.1",
+      build_time : number = 1670210112706;
 
 export { version, build_time };
