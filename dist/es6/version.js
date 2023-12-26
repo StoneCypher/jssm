7506dede45ac1d8745109d7d998bcd54421af38a
John Haugeland <stonecypher@gmail.com>
2023-12-26 11:59:09 -0800

    set version to 5.91.0

--- dist/es6/version.js
+++ dist/es6/version.js
@@ -1,2 +1,2 @@
-const version = "5.91.3", build_time = 1703620218243;
+const version = "5.91.0", build_time = 1703620444124;
 export { version, build_time };
--- src/ts/version.ts
+++ src/ts/version.ts
@@ -1,5 +1,5 @@
 
-const version    : string = "5.91.3",
-      build_time : number = 1703620218243;
+const version    : string = "5.91.0",
+      build_time : number = 1703620444124;
 
 export { version, build_time };
