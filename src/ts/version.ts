cbb3d689578b29108c483132721c596637e7af61
John Haugeland <stonecypher@gmail.com>
2024-10-20 11:11:03 -0700

    Accept @tylerbutler 's generous donation; test locally for nine years; bump version, rebuild, release

--- src/ts/version.ts
+++ src/ts/version.ts
@@ -1,5 +1,5 @@
 
-const version    : string = "5.99.0",
-      build_time : number = 1720314479976;
+const version    : string = "5.100.0",
+      build_time : number = 1729447735226;
 
 export { version, build_time };
