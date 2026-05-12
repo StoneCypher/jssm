215cad3ab26f0fffc4734b11f0e06bf1add9f684
John Haugeland <stonecypher@gmail.com>
2026-05-12 12:41:10 -0700

    build: rebuild dist after merge of main into stonecypher/fsl#1328

--- src/ts/version.ts
+++ src/ts/version.ts
@@ -12,6 +12,6 @@ const version    : string = "5.112.7";
  *  written by `src/buildjs/makever.cjs`.  Useful for distinguishing builds
  *  with the same `version` string during development, and for diagnostic logs.
  */
-const build_time : number = 1778613415398;
+const build_time : number = 1778614632161;
 
 export { version, build_time };
