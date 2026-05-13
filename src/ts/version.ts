a8c58a2f029bb6a998b7014883db0e60409cc80b
John Haugeland <stonecypher@gmail.com>
2026-05-13 08:27:04 -0700

    version bumped

--- src/ts/version.ts
+++ src/ts/version.ts
@@ -12,6 +12,6 @@ const version    : string = "5.113.0";
  *  written by `src/buildjs/makever.cjs`.  Useful for distinguishing builds
  *  with the same `version` string during development, and for diagnostic logs.
  */
-const build_time : number = 1778644202102;
+const build_time : number = 1778685151087;
 
 export { version, build_time };
